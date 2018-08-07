b7d838eae6af30808ac4914cdb96ab9e80c05700
Ales Kocjancic <alesdotio@gmail.com>
2018-08-07 16:00:21 +0200

    overview

--- src/ClusterHealth.jsx
+++ src/ClusterHealth.jsx
@@ -19,9 +19,14 @@ var ClusterHealth = React.createClass({
 			<div className={clusterClass}>
 				<h1 className="cluster-name">{this.props.health.cluster_name}</h1>
 				<ul className="cluster-stats">
-					<li>Relocating: {this.props.health.relocating_shards}</li>
-					<li>Unassigned: {this.props.health.unassigned_shards}</li>
+					<li>Data nodes: {this.props.health.number_of_data_nodes}</li>
+					<li>Primary shards: {this.props.health.active_primary_shards}</li>
+					<li>Active shards: {this.props.health.active_shards}</li>
+				</ul>
+				<ul className="cluster-stats">
 					<li className={this.props.health.number_of_pending_tasks > 150 ? 'text-danger': ''}>Pending tasks: {this.props.health.number_of_pending_tasks}</li>
+					<li>Relocating shards: {this.props.health.relocating_shards}</li>
+					<li>Unassigned shards: {this.props.health.unassigned_shards}</li>
 				</ul>
 			</div>
 		);
