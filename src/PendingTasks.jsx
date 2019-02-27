a009f67aa2de4ec1ddab32fec067bdab2e6d7a53
Ales Kocjancic <alesdotio@gmail.com>
2019-02-27 06:37:43 +0100

    improved styles

--- src/PendingTasks.jsx
+++ src/PendingTasks.jsx
@@ -14,29 +14,32 @@ var PendingTasks = React.createClass({
 			return <div>Loading...</div>;
 		}
 		return (
-			<table className="tasks">
-				<colgroup>
-					<col width="20%" />
-					<col width="20%" />
-					<col width="60%" />
-				</colgroup>
-				<thead>
-				<tr>
-					<th>Priority</th>
-					<th>Time in queue</th>
-					<th>Source</th>
-				</tr>
-				</thead>
-				<tbody>
-				{this.props.pendingTasks.map(function(task, i) {
-					return <tr key={i}>
-						<td>{task.priority}</td>
-						<td>{task.time_in_queue}</td>
-						<td>{task.source}</td>
+			<div className="tasks">
+				<h3>Pending tasks</h3>
+				<table className="tasks-table">
+					<colgroup>
+						<col width="20%" />
+						<col width="20%" />
+						<col width="60%" />
+					</colgroup>
+					<thead>
+					<tr>
+						<th>Priority</th>
+						<th>Time in queue</th>
+						<th>Source</th>
 					</tr>
-				})}
-				</tbody>
-			</table>
+					</thead>
+					<tbody>
+					{this.props.pendingTasks.map(function(task, i) {
+						return <tr key={i}>
+							<td>{task.priority}</td>
+							<td>{task.time_in_queue}</td>
+							<td>{task.source}</td>
+						</tr>
+					})}
+					</tbody>
+				</table>
+			</div>
 		);
 	}
 
